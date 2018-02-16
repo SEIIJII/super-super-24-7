dc45adffecde325ab2640df39880be74fbebae94
SEIIJII <36533721+SEIIJII@users.noreply.github.com>
2018-02-16 11:19:19 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "24/7",
+  "description": "24/7",
+  "version": "0.0.0",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+    
+  },
+  "dependencies": {
+    "discord.js": "11.1.0",
+    "request": "2.81.0"
+  }
+}
